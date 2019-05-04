dee98e04d78f857024bf24c7a4bd847f70aac54c
Kexin6 <30775651+Kexin6@users.noreply.github.com>
2019-05-04 19:57:54 -0400

    Update 15.cpp

--- 15.cpp
+++ 15.cpp
@@ -3,6 +3,7 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<int> oneInNums;
         vector<vector<int>> returnNum;
+        bool canReturn = true;
         for (int i = 0; i < nums.size(); i ++) {
             for (int j = i + 1; j < nums.size(); j ++) {
                 for (int k = j + 1; k < nums.size(); k ++) {
@@ -10,8 +11,16 @@ public:
                         oneInNums.push_back(nums[i]);
                         oneInNums.push_back(nums[j]);
                         oneInNums.push_back(nums[k]);
-                        returnNum.push_back(oneInNums);
+                        std::sort (oneInNums.begin(), oneInNums.end());
+                        
+                        for (int i = 0; i < returnNum.size(); i ++) {
+                            if (oneInNums == returnNum[i]) {
+                                canReturn = false;
+                            } 
+                        }
+                        if (canReturn) returnNum.push_back(oneInNums);
                         oneInNums.clear();
+                        canReturn = true;
                     }
                 }
             }
